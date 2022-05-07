086b274ce6b0c58a0663cf68738df504d6eebba5
Towsif Aktar <100977467+cryptob31@users.noreply.github.com>
2022-05-07 20:00:58 +0600

    Update crypto_generation_display.ino
    Author: Crypto Generation 
      Platforms: ESP8266
      Language: C++/Arduino
      File: crypto_generation_display.ino
      ------------------------------------------------------------------------------
      Description: 
      Code for YouTube video (https://youtube.com/shorts/Oxa0HVVbBIo?feature=share) demonstrating how to use the coingecko API to fetch the 
      price of Multiple cryptocurrency and display it on a 0.96" OLED over I2C.
      ------------------------------------------------------------------------------
      Recommendation :
      I recommend to use Pre-compiled >> crypto_generation_display_d1_mini.bin
      
      
     Download ESPRESSIF ESP flash tool: https://www.espressif.com/sites/default/files/tools/flash_download_tool_3.9.2.zip
      ------------------------------------------------------------------------------
    OLED Display SSD1306 Pin Wiring.
    Pin -->	ESP8266
    Vin -->	3.3V
    GND	--> GND
    SCL	--> GPIO 5 (D1)
    SDA	--> GPIO 4 (D2)

--- crypto_generation_display/crypto_generation_display.ino
+++ crypto_generation_display/crypto_generation_display.ino
@@ -6,18 +6,21 @@
   File: crypto_generation_display.ino
   ------------------------------------------------------------------------------
   Description: 
-  Code for YouTube video demonstrating how to use the coingecko API to fetch the 
+  Code for YouTube video (https://youtube.com/shorts/Oxa0HVVbBIo?feature=share) demonstrating how to use the coingecko API to fetch the 
   price of Multipe cryptocurrency and display it on a 0.96" OLED over I2C.
   ------------------------------------------------------------------------------
-  License:
-  Please see attached LICENSE.txt file for details.
+  Recommendation :
+  I recommend to use Pre-compiled >> crypto_generation_display_d1_mini.bin
+  
+  
+ Download ESPRESSIF ESP flash tool: https://www.espressif.com/sites/default/files/tools/flash_download_tool_3.9.2.zip
   ------------------------------------------------------------------------------
 OLED Display SSD1306 Pin Wiring.
-Pin	ESP8266
-Vin	3.3V
-GND	GND
-SCL	GPIO 5 (D1)
-SDA	GPIO 4 (D2)
+Pin -->	ESP8266
+Vin -->	3.3V
+GND	--> GND
+SCL	--> GPIO 5 (D1)
+SDA	--> GPIO 4 (D2)
 ------------------------------------------------------------------------------*/
 
 #include <ESP8266WiFi.h>
